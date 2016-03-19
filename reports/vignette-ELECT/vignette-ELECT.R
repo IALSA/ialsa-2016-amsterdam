2153f3d23796604d862bdc6f23996f06465b030f
Andrey Koval <koval.andrey@gmail.com>
2016-03-19 08:20:09 -0700

    templates and development

---
+++ reports/vignette-ELECT/vignette-ELECT.R
@@ -0,0 +1,33 @@
+# knitr::stitch_rmd(script="./vignette-ELECT/vignette-ELECT.R", output="./vignette-ELECT/vignette-ELECT/a.md")
+#These first few lines run only when the file is run in RStudio, !!NOT when an Rmd/Rnw file calls it!!
+rm(list=ls(all=TRUE))  #Clear the variables from previous runs.
+cat("\f") # clear console 
+
+# ---- load-sources ------------------------------------------------------------
+# Call `base::source()` on any repo file that defines functions needed below.  Ideally, no real operations are performed.
+source("./scripts/common-functions.R") # used in multiple reports
+source("./scripts/graph-presets.R") # fonts, colors, themes 
+
+# ---- load-packages -----------------------------------------------------------
+# Attach these packages so their functions don't need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+library(magrittr) # enables piping : %>% 
+
+# Verify these packages are available on the machine, but their functions need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+requireNamespace("ggplot2") # graphing
+# requireNamespace("readr") # data input
+requireNamespace("tidyr") # data manipulation
+requireNamespace("dplyr") # Avoid attaching dplyr, b/c its function names conflict with a lot of packages (esp base, stats, and plyr).
+requireNamespace("testit")# For asserting conditions meet expected patterns.
+# requireNamespace("car") # For it's `recode()` function.
+
+# ---- declare-globals ---------------------------------------------------------
+path_data_example_i <- "./data/shared/raw/dataExample1.RData"
+# ---- load-data ---------------------------------------------------------------
+load(path_data_example_i)
+ds <- data
+dplyr::tbl_df(ds)
+
+# ---- inspect-data -------------------------------------------------------------
+
+# ---- tweak-data --------------------------------------------------------------
+
