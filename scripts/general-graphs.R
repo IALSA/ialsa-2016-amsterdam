de0c8816cef154608d6c83e4890f79b68fd3364b
Andrey Koval <koval.andrey@gmail.com>
2016-04-12 14:57:12 -0700

    update

--- scripts/general-graphs.R
+++ scripts/general-graphs.R
@@ -5,7 +5,8 @@ basic_line <- function(
   color_name="black",
   line_alpha=1,
   line_size =.5, 
-  smoothed = FALSE,
+  # smoothed = FALSE,
+  smoother = FALSE, # lm, loess
   main_title     = variable_name,
   x_title        = paste0("Time metric: ", time_metric),
   y_title        = variable_name,
@@ -16,7 +17,14 @@ basic_line <- function(
   d_observed <- d_observed[!base::is.na(d_observed[, variable_name]), ]
   
   g <- ggplot(d_observed, aes_string(x=time_metric, y = variable_name)) 
-  if(!smoothed){
+  # if(!smoothed){
+  #   g <- g + geom_line(aes_string(group="id"), size=line_size, color=alpha(color_name,line_alpha), na.rm=T)   
+  # } else{
+  #   g <- g + geom_smooth(aes_string(group="id"),size=line_size,  method="lm",color=alpha(color_name,line_alpha), na.rm=T, se=F )
+  #   g <- g + geom_smooth(method="loess", color="blue", size=1, fill="gray80", alpha=.3, na.rm=T)
+  #   
+  # }  
+  ife (!smoothed){
     g <- g + geom_line(aes_string(group="id"), size=line_size, color=alpha(color_name,line_alpha), na.rm=T)   
   } else{
     g <- g + geom_smooth(aes_string(group="id"),size=line_size,  method="lm",color=alpha(color_name,line_alpha), na.rm=T, se=F )
