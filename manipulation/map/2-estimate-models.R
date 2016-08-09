9343adfa1e660b1a5068c9a64c0ff98071c6a703
Andrey Koval <koval.andrey@gmail.com>
2016-08-09 16:54:44 -0700

    stabilized estimation function
    todo: automate LE estimation into the same list object

--- manipulation/map/2-estimate-models.R
+++ manipulation/map/2-estimate-models.R
@@ -75,172 +75,149 @@ ds_clean <- ds_ms %>%
 
 # begin modeling
 ds <- ds_clean
-cat("\nState table:"); print(msm::statetable.msm(state,id,data=ds))
 ds %>% dplyr::summarise(unique_ids = n_distinct(id)) # subject count
 ds %>% dplyr::group_by(state) %>% dplyr::summarize(count = n()) # basic frequiencies
+cat("\nState table:"); print(msm::statetable.msm(state,id,data=ds)) # transition frequencies
+
+
 # NOTE: -2 is a right censored value, indicating being alive but in an unknown living state.
 
-(N <- length(unique(ds$id)))
-subjects <- as.numeric(unique(ds$id))
 
-# Add first observation indicator
-# this creates a new dummy variable "firstobs" with 1 for the first wave
-cat("\nFirst observation indicator is added.\n")
-offset <- rep(NA,N)
-for(i in 1:N){offset[i] <- min(which(ds$id==subjects[i]))}
-firstobs <- rep(0,nrow(ds))
-firstobs[offset] <- 1
-ds <- cbind(ds,firstobs=firstobs)
-head(ds)
-
-# Time intervals in data:
-# the age difference between timepoint for each individual
-intervals <- matrix(NA,nrow(ds),2)
-for(i in 2:nrow(ds)){
-  if(ds$id[i]==ds$id[i-1]){
-    intervals[i,1] <- ds$id[i]
-    intervals[i,2] <- ds$age[i]-ds$age[i-1]
-  }
-  intervals <- as.data.frame(intervals)
-  colnames(intervals) <- c("id", "interval")
-}
-head(intervals)
-
-# the age difference between timepoint for each individual
-# Remove the N NAs:
-intervals <- intervals[!is.na(intervals[,2]),]
-cat("\nTime intervals between observations within individuals:\n")
-print(round(quantile(intervals[,2]),digits))
-
-# Info on age and time between observations:
-opar<-par(mfrow=c(1,3), mex=0.8,mar=c(5,5,3,1))
-hist(ds$age[ds$firstobs==1],col="red",xlab="Age at baseline in years",main="")
-hist(ds$age,col="blue",xlab="Age in data in years",main="") 
-hist(intervals[,2],col="green",xlab="Time intervals in data in years",main="") 
-opar<-par(mfrow=c(1,1), mex=0.8,mar=c(5,5,2,1))
+# ---- ages-and-intervals -------------------------------------
+# (N <- length(unique(ds$id)))
+# subjects <- as.numeric(unique(ds$id))
+# # Add first observation indicator
+# # this creates a new dummy variable "firstobs" with 1 for the first wave
+# cat("\nFirst observation indicator is added.\n")
+# offset <- rep(NA,N)
+# for(i in 1:N){offset[i] <- min(which(ds$id==subjects[i]))}
+# firstobs <- rep(0,nrow(ds))
+# firstobs[offset] <- 1
+# ds <- cbind(ds,firstobs=firstobs)
+# head(ds)
+# 
+# # Time intervals in data:
+# # the age difference between timepoint for each individual
+# intervals <- matrix(NA,nrow(ds),2)
+# for(i in 2:nrow(ds)){
+#   if(ds$id[i]==ds$id[i-1]){
+#     intervals[i,1] <- ds$id[i]
+#     intervals[i,2] <- ds$age[i]-ds$age[i-1]
+#   }
+#   intervals <- as.data.frame(intervals)
+#   colnames(intervals) <- c("id", "interval")
+# }
+# head(intervals)
+# 
+# # the age difference between timepoint for each individual
+# # Remove the N NAs:
+# intervals <- intervals[!is.na(intervals[,2]),]
+# cat("\nTime intervals between observations within individuals:\n")
+# print(round(quantile(intervals[,2]),digits))
+# 
+# # Info on age and time between observations:
+# opar<-par(mfrow=c(1,3), mex=0.8,mar=c(5,5,3,1))
+# hist(ds$age[ds$firstobs==1],col="red",xlab="Age at baseline in years",main="")
+# hist(ds$age,col="blue",xlab="Age in data in years",main="") 
+# hist(intervals[,2],col="green",xlab="Time intervals in data in years",main="") 
+# opar<-par(mfrow=c(1,1), mex=0.8,mar=c(5,5,2,1))
 
 
 # ---- explore-msm -----------------------------------------------
+# load utility functions
+source("./scripts/ELECT-utility-functions.R")
 # define function for getting a simple multistate output
-# Choose model (0/1/2):
-# Model <- 2
-q = .01
-qnames = c("q12","q21","q13","q14","q24", "q34", "q23", "q31")
-method = "BFGS"
-# cov_names = "1 + age_bl"
 
-simple_multistate <- function(ds, q, qnames, method, cov_names){
-  covariates <- as.formula(paste0("~",cov_names))
-  constraint <- NULL
-  fixedpars <- NULL
-  (Q <- rbind(c(0,q,q,q), c(q,0,q,q),c(q,q,0,0), c(0,0,0,0)))
-  model <- msm(
-    formula       = state ~ age, 
-    subject       = id, 
-    data          = ds, 
-    center        = FALSE,
-    qmatrix       = Q, 
-    death         = TRUE, 
-    covariates    = covariates,
-    censor        = c(-1,-2), 
-    censor.states = list(c(1,2,3), c(1,2,3)), 
-    method        = method,
-    constraint    = constraint,
-    fixedpars     = fixedpars,
-    control       = list(trace=0,REPORT=1,maxit=1000,fnscale=10000)
-  )
-  return(model)
-    # Generate output:
-  cat("\n---------------------------------------\n")
-  cat("\nModel","---"," with covariates: "); print(covariates)
-  cat("and constraints:\n"); print(constraint)
-  cat("and fixedpars:\n"); print(fixedpars)
-  cat("Convergence code =", model$opt$convergence,"\n")
-  minus2LL <-  model$minus2loglik
-  AIC <- minus2LL + 2*length(model$opt$par)
-  cat("\n-2loglik =", minus2LL,"\n")
-  cat("AIC =", AIC,"\n")
-  p <- round(model$estimates, 2)
-  p.se <- round(sqrt(diag(model$covmat)),2)
-  # Univariate Wald tests:
-  wald <- round((p/p.se)^2,digits)
-  pvalue <- round(1-pchisq((p/p.se)^2,df=1),digits)
-  # Do not test intercepts:
-  wald[1:sum(names(p)=="qbase")] <- wald
-  pvalue[1:sum(names(p)=="qbase")] <- pvalue
-  # Results to screen:
-  cat("\nParameter estimats and SEs:\n")
-  print(cbind(q=qnames,p=round(p,digits),
-              se=round(p.se,digits),"Wald ChiSq"=wald,
-              "Pr>ChiSq"=pvalue),quote=FALSE)
-  cat("\n---------------------------------------\n")
-  
-}
-
-ds <- ds %>% 
+ds <- ds_clean %>% 
   dplyr::mutate(
-    # male = as.numeric(male),
+    male = as.numeric(male),
     age    = age - 75,
     age_bl = age_bl - 75
   )
 
-# m0 <- simple_multistate(ds, q, qnames, method,  cov_names = "1")
-# m1 <- simple_multistate(ds, q, qnames, method,  cov_names = "age")
-# m2 <- simple_multistate(ds, q, qnames, method,  cov_names = "age + age_bl")
-# m3 <- simple_multistate(ds, q, qnames, method,  cov_names = "age + age_bl + male")
-# m4 <- simple_multistate(ds, q, qnames, method,  cov_names = "age + age_bl + male + edu")
+q = .01
+# qnames <- c("q12", "q13","q14","q21", "q23", "q24","q32", "q34")
+qnames = c(
+  "Healthy - Mild",  # q12
+  "Health - Severe", # q13
+  "Healthy - Dead",  # q14
+  "Mild - Healthy",  # q21  
+  "Mild - Severe",   # q23
+  "Mild - Dead",     # q24
+  "Severe - Mild",   # q32
+  "Severe - Dead"    # q34
+  )
+(Q <- rbind(c(0,q,q,q), c(q,0,q,q),c(0,q,0,q), c(0,0,0,0))) # verify structure
+
+
+# m1 <- estimate_multistate(
+#   ds = ds,
+#   Q = Q,
+#   qnames = qnames,
+#   cov_names = "age",
+#   method = "BFGS"
+# )
+
+# m0 <- estimate_multistate(ds, q, qnames, method,  cov_names = "1")
+# m1 <- estimate_multistate(ds, Q, qnames,cov_names = "age")
+# m2 <- estimate_multistate(ds, Q, qnames,cov_names = "age + age_bl")
+# m3 <- estimate_multistate(ds, Q, qnames,cov_names = "age + age_bl + male")
+# m4 <- estimate_multistate(ds, Q, qnames,cov_names = "age + age_bl + male + edu")
+
+examine_multistate(m1)
+# ---- msm-save-to-disk ----------------
+# save estimated models in a external object for faster access in the future
+
+# models <- list(
+#   "empty"  = m0,
+#   "age"    = m1,
+#   "age_bl" = m2,
+#   "male"   = m3,
+#   "edu"    = m4
+# )
 # 
-
-
-# models 3 and 4 produced the following warning message:
-# Optimisation has probably not converged to the maximum likelihood - Hessian is not positive definite.
-models <- list(
-  "empty"  = m0,
-  "age"    = m1,
-  "age_bl" = m2,
-  "male"   = m3,
-  "edu"    = m4
-)
-
 # saveRDS(models, "./data/shared/derived/models.rds")
 models <- readRDS("./data/shared/derived/models.rds")
 
 # ---- compute-LE -------------------
+alive_states <- c(1,2,3)
+ds_alive <- ds[ds$state %in% alive_states,]
 
-# ---- three-functions-of-ELECT -------------
-dta = ds; model = models$age
-# graph_LE <- function(dta, model){
-  # Point-estimate life expectancies:
-  sddata <- dta[dta$state%in%c(1,2,3),]
-  age_0 <- -0
-  ageMax <- 50
+age_min <- -20
+age_max <- 50
+replication_n <- 50
+time_scale <- "years"
+grid_par <- .5
 
-  LE.pnt <- elect(
-    model=model, # fitted msm model
-    b.covariates=list(age=age_0), # list with specified covarites values
-    statedistdata=sddata, # data for distribution of living states
-    time.scale.msm="years", # time scale in multi-state model ("years", ...)
-    h=0.5, # grid parameter for integration
-    age.max=ageMax, # assumed maximum age in years
-    S=500 # number of simulation cycles
+# estimate_LE <- function(ds, model, alive_states, age_min, age_max, covar_values){
+
+LE_1 <- elect(
+    model=m1, # fitted msm model
+    b.covariates=list(age=age_min), # list with specified covarites values
+    statedistdata=ds_alive, # data for distribution of living states
+    time.scale.msm=time_scale, # time scale in multi-state model ("years", ...)
+    h=grid_par, # grid parameter for integration
+    age.max=age_max, # assumed maximum age in years
+    S=replication_n # number of simulation cycles
   )
+
+summary.elect(
+  LE_1, # life expectancy estimated by elect()
+  probs = c(.025, .5, .975), # numeric vector of probabilities for quantiles
+  digits=2, # number of decimals places in output
+  print = TRUE # print toggle
+)
+
+plot.elect(
+  LE_1, # life expectancy estimated by elect()
+  kernel = "gaussian", #character string for smoothing kernal ("gaussian",...)
+  col = "red", # color of the curve
+  lwd = 2, # line width of the curve
+  cex.lab = 1 # magnification to be used for axis-labels
+)
+
   
-  summary.elect(
-    LE.pnt, # life expectancy estimated by elect()
-    probs = c(.025, .5, .975), # numeric vector of probabilities for quantiles
-    digits=2, # number of decimals places in output
-    print = TRUE # print toggle
-  )
-  
-  plot.elect(
-    LE.pnt, # life expectancy estimated by elect()
-    kernel = "gaussian", #character string for smoothing kernal ("gaussian",...)
-    col = "red", # color of the curve
-    lwd = 2, # line width of the curve
-    cex.lab = 1 # magnification to be used for axis-labels
-  )
-  
-# }
+
 
 
 
