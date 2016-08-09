9a1d5fea531b0513e2f1bc74b23cd34f2e46b6ea
Andrey Koval <koval.andrey@gmail.com>
2016-08-09 16:55:03 -0700

    #23 basic utility function is ready

---
+++ scripts/ELECT-utility-functions.R
@@ -0,0 +1,78 @@
+# this script provide some utility functions to facilitate the use of ELECT
+# note that in the current state is covers ONLY the case with 4 categories
+
+#---- msm-estimation -----------------
+estimate_multistate <- function(
+  ds,                # data object 
+  Q,                 # Q-matrix of transitions
+  qnames,            # names of the rows in the Q matrix
+  cov_names,         # string with covariate names
+  method_ = "BFGS",  # alternatively, if does not converge "Nedler-Mead"
+  constraint = NULL, # additional model constraints
+  fixedpars = NULL   # fixed parameters
+){
+  covariates <- as.formula(paste0("~",cov_names))
+  model <- msm(
+    formula       = state ~ age, 
+    subject       = id, 
+    data          = ds, 
+    center        = FALSE,
+    qmatrix       = Q, 
+    death         = TRUE, 
+    covariates    = covariates,
+    censor        = c(-1,-2), 
+    censor.states = list(c(1,2,3), c(1,2,3)), 
+    method        = method_,
+    constraint    = constraint,
+    fixedpars     = fixedpars,
+    control       = list(trace=0,REPORT=1,maxit=1000,fnscale=10000)
+  )
+  return(model)
+} 
+
+# use of the function
+# note: make sure to center age and other predictors before running
+# m1 <- estimate_multistate(
+#   ds = ds,
+#   Q = Q,
+#   qnames = qnames,
+#   cov_names = "age",
+#   method = "BFGS"
+# )
+
+# ---- msm-examination ----------------------
+examine_multistate <- function(model, digits=3){
+  # Generate output:
+  cat("\n---------------------------------------\n")
+  cat("\nModel","---"," with covariates: "); print(model$covariates, showEnv=F)
+  cat("and constraints:\n"); print(model$constraint)
+  cat("and fixedpars:\n"); print(model$fixedpars)
+  cat("Convergence code =", model$opt$convergence,"\n")
+  minus2LL <-  model$minus2loglik
+  AIC <- minus2LL + 2*length(model$opt$par)
+  cat("\n-2loglik =", minus2LL,"\n")
+  cat("AIC =", AIC,"\n")
+  p <- as.numeric(round(model$estimates, digits))
+  p.se <- as.numeric(round(sqrt(diag(model$covmat)),digits))
+  # Univariate Wald tests:
+  wald <- round((p/p.se)^2,digits)
+  pvalue <- round(1-pchisq((p/p.se)^2,df=1),digits)
+  # Do not test intercepts:
+  wald[1:sum(names(p)=="qbase")] <- wald
+  pvalue[1:sum(names(p)=="qbase")] <- pvalue
+  # Results to screen:
+  # model_results <- cbind(q=qnames,p=round(p,digits),
+  #                        se=round(p.se,digits),"Wald ChiSq"=wald,
+  #                        "Pr>ChiSq"=pvalue)
+  model_results <- data.frame(
+    q=qnames,p=round(p,digits),
+    se=round(p.se,digits),"Wald ChiSq"=wald,
+    "Pr>ChiSq"=pvalue
+  )
+  cat("\nParameter estimats and SEs:\n")
+  cat("\n---------------------------------------\n")
+  # print(model_results, quote = FALSE)
+  # print(knitr::kable(model_results))
+  return(model_results)
+}
+# (a <- examine_multistate(m1))
