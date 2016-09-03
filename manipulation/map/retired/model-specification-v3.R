5d9be1b24f2edf769224e8f31e85eae403f07dd0
Andrey Koval <koval.andrey@gmail.com>
2016-09-03 14:52:23 -0700

    reorganize

---
+++ manipulation/map/retired/model-specification-v3.R
@@ -0,0 +1,86 @@
+# Specification of the fitted models
+
+# common value for all models
+q <- .01
+
+# ---- model-A --------------
+# transition matrix
+Q <- rbind( c(0, q, 0, q), 
+            c(q, 0, q, q),
+            c(0, 0, 0, q), 
+            c(0, 0, 0, 0)) 
+# misclassification matrix
+E <- rbind( c( 0,  0,  0, 0),  
+            c( 0,  0, .1, 0), 
+            c( 0,  0,  0, 0),
+            c( 0,  0,  0, 0) )
+# transition names
+qnames = c(
+  "Healthy - Mild",  # q12
+  # "Healthy - Severe", # q13
+  "Healthy - Dead",  # q14
+  "Mild - Healthy",  # q21  
+  "Mild - Severe",   # q23
+  "Mild - Dead",     # q24
+  # "Severe - Healthy",# q31
+  # "Severe - Mild",   # q32
+  "Severe - Dead"    # q34
+)
+A <- list(q=q, Q=Q, E=E, qnames = qnames)
+
+# ---- model-B --------------
+# transition matrix
+Q <- rbind( c(0, q, 0, q), 
+            c(q, 0, q, q),
+            c(0, q, 0, q), 
+            c(0, 0, 0, 0)) 
+# misclassification matrix
+E <- rbind( c( 0,  0,  0, 0),  
+            c( 0,  0,  0, 0), 
+            c( 0,  0,  0, 0),
+            c( 0,  0,  0, 0) )
+# transition names
+qnames = c(
+  "Healthy - Mild",  # q12
+  # "Healthy - Severe", # q13
+  "Healthy - Dead",  # q14
+  "Mild - Healthy",  # q21  
+  "Mild - Severe",   # q23
+  "Mild - Dead",     # q24
+  # "Severe - Healthy",# q31
+  "Severe - Mild",   # q32
+  "Severe - Dead"    # q34
+)
+B <- list(q=q, Q=Q, E=E, qnames = qnames)
+
+
+# ---- model-C --------------
+# transition matrix
+Q <- rbind( c(0, q, 0, q), 
+            c(q, 0, q, q),
+            c(0,.05,0, q), 
+            c(0, 0, 0, 0)) 
+# misclassification matrix
+E <- rbind( c( 0,  0,  0, 0),  
+            c( 0,  0,  0, 0), 
+            c( 0,  0,  0, 0),
+            c( 0,  0,  0, 0) )
+# transition names
+qnames = c(
+  "Healthy - Mild",  # q12
+  # "Healthy - Severe", # q13
+  "Healthy - Dead",  # q14
+  "Mild - Healthy",  # q21  
+  "Mild - Severe",   # q23
+  "Mild - Dead",     # q24
+  # "Severe - Healthy",# q31
+  "Severe - Mild",   # q32
+  "Severe - Dead"    # q34
+)
+
+C <- list(q=q, Q=Q, E=E, qnames = qnames)
+
+
+# ---- assemble-specification-object ------------
+
+model_specification <- list(A=A, B=B, C=C)
