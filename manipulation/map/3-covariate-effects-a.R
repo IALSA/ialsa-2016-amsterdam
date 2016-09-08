db5d259aa4831427f16ee323e9f2af3cffc9de78
Andrey Koval <koval.andrey@gmail.com>
2016-09-08 16:49:48 -0700

    #34 work update

--- manipulation/map/3-covariate-effects-a.R
+++ manipulation/map/3-covariate-effects-a.R
@@ -19,15 +19,28 @@ print(pathSaveFolder)
 
 
 # ---- load-data ---------------------------------------------------------------
+ds_clean <- readRDS("./data/unshared/ds_clean.rds")
 # assemble the list object with the results of msm estimation
+pathSaveFolder <- "./data/shared/derived/models/model-a/"
 models <- list()
 models[["age"]][["msm"]]    <- readRDS(paste0(pathSaveFolder,'mA1.rds'))
 models[["age_bl"]][["msm"]] <- readRDS(paste0(pathSaveFolder,'mA2.rds'))
 models[["male"]][["msm"]]   <- readRDS(paste0(pathSaveFolder,'mA3.rds'))
 models[["educat"]][["msm"]] <- readRDS(paste0(pathSaveFolder,'mA4.rds'))
+mA <- models
 
+# assemble the list object with the results of msm estimation
+pathSaveFolder <- "./data/shared/derived/models/model-b/"
+models <- list()
+models[["age"]][["msm"]]    <- readRDS(paste0(pathSaveFolder,'mB1.rds'))
+models[["age_bl"]][["msm"]] <- readRDS(paste0(pathSaveFolder,'mB2.rds'))
+models[["male"]][["msm"]]   <- readRDS(paste0(pathSaveFolder,'mB3.rds'))
+models[["educat"]][["msm"]] <- readRDS(paste0(pathSaveFolder,'mB4.rds'))
+mB <- models
 
 # ---- inspect-data -------------------------------------------------------------
+summary(ds_clean$age)
+summary(ds_clean$age_bl)
 
 #---- define-support-functions ----------------------
 
@@ -42,32 +55,66 @@ print(cbind(p=round(p,digits),
       quote=FALSE)
 }
 
+msm_details <- function(model){ 
+  # intensity matrix
+  cat("\n Intensity matrix : \n")
+  print(qmatrix.msm(model)) 
+  # qmatrix.msm(model, covariates = list(male = 0))
+  # transition probability matrix
+  t_ <- 2
+  cat("\n Transition probability matrix for t = ", t_," : \n")
+  print(pmatrix.msm(model, t = t_)) # t = time, in original metric
+  # misclassification matrix
+  cat("\n Misclassification matrix : \n")
+  suppressWarnings(print(ematrix.msm(model), warnings=F))
+  # mean sojourn times
+  cat("\n Mean sojourn times : \n")
+  print(sojourn.msm(model))
+  # probability that each state is next
+  cat("\n Probability that each state is next : \n")
+  suppressWarnings(print(pnext.msm(model)))
+  # total length of stay
+  cat("\n  Total length of stay : \n")
+  print(totlos.msm(model))
+  # expected number of visits to the state
+  cat("\n Expected number of visits to the state : \n")
+  suppressWarnings(print(envisits.msm(model)))
+  # ratio of transition intensities
+  # qratio.msm(model,ind1 = c(2,1), ind2 = c(1,2))
+}
 
 
-# ---- specify-model --------------------------
-q <- .01
-# transition matrix
-Q <- rbind( c(0, q, 0, q), 
-            c(q, 0, q, q),
-            c(0, q, 0, q), 
-            c(0, 0, 0, 0)) 
-# misclassification matrix
-E <- rbind( c( 0,  0,  0, 0),  
-            c( 0,  0,  0, 0), 
-            c( 0,  0,  0, 0),
-            c( 0,  0,  0, 0) )
-# transition names
-qnames = c(
-  "Healthy - Mild",   # q12
-  # "Healthy - Severe", # q13
-  "Healthy - Dead",  # q14
-  "Mild - Healthy",  # q21  
-  "Mild - Severe",   # q23
-  "Mild - Dead",     # q24
-  # "Severe - Healthy",# q31
-  "Severe - Mild",   # q32
-  "Severe - Dead"    # q34
-)
+# ----- le-general-function ---------------------------
+model <- models
+
+
+le_spread <- function(
+  msm_model, 
+  age_min, 
+  age_max, 
+  ds_levels,
+  condition_n
+){
+  # assemble the levels of the covariates
+  covar_list <- list(
+    age    = age_min,
+    age_bl = ds_levels[condition_n,"age_bl"],
+    male   = ds_levels[condition_n, "male"],
+    educat = ds_levels[condition_n, "educat"]
+  )
+  # estimate Life Expectancies
+  LE <- elect(
+    model          = msm_model, # fitted msm model
+    b.covariates   = covar_list, # list with specified covarites values
+    statedistdata  = ds_alive, # data for distribution of living states
+    time.scale.msm = time_scale, # time scale in multi-state model ("years", ...)
+    h              = grid_par, # grid parameter for integration
+    age.max        = age_max, # assumed maximum age in years
+    S              = replication_n # number of simulation cycles
+  )
+  return(LE)
+}
+
 
 # ---- assemble-covariate-levels ----------------------
 age_bl_possible <- seq(from=-20, to=20, by= 5) 
@@ -89,9 +136,17 @@ educat <- ds_levels[1,"educat"]
 alive_states <- c(1,2,3)
 ds_alive <- ds[ds$state %in% alive_states,]
 fixedpars <- fixedpars_
-age_min <- -20
-age_max <- 35
-age_bl <- 0
+
+
+summary(ds_clean$age)
+summary(ds_clean$age_bl)
+
+# age centered at 75
+# age_bl centered at 75 
+
+age_min <- 0 # 75
+age_max <- 35 # 110
+age_bl <- -20 # 55
 male <- 0
 educat <- 0
 
@@ -99,6 +154,41 @@ educat <- 0
 replication_n <- 1000
 time_scale <- "years"
 grid_par <- .5
+
+covar_list = list(age=age_min, age_bl=age_bl, male=male, educat=educat)
+
+LE <- elect(
+  model          = msm_model, # fitted msm model
+  b.covariates   = covar_list, # list with specified covarites values
+  statedistdata  = ds_alive, # data for distribution of living states
+  time.scale.msm = time_scale, # time scale in multi-state model ("years", ...)
+  h              = grid_par, # grid parameter for integration
+  age.max        = age_max, # assumed maximum age in years
+  S              = replication_n # number of simulation cycles
+)
+
+
+
+
+
+
+
+
+
+
+
+
+A <- models
+
+msm <- models$educat$msm
+m <- les$`1`
+
+summary.elect(
+  m, # life expectancy estimated by elect()
+  probs = c(.025, .5, .975), # numeric vector of probabilities for quantiles
+  digits=2, # number of decimals places in output
+  print = TRUE # print toggle
+)
 
 
 conditions <- list()
@@ -132,6 +222,8 @@ for(i in 1:nrow(ds_levels)){
   les[[paste0(as.character(i))]] <- one_le(models$educat$msm, 0, 35, ds_levels, i)  
 
 }
+
+
 
 
 # ---- elect-general-function ---------------
